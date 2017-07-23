71a9afbb95e33ff20db9f8740146dbe0907eb68f
foxneZz <foxnezz@gmail.com>
2017-07-23 17:47:37 +0300

    added index controller placeholders / removed users

---
+++ controllers/indexController.js
@@ -0,0 +1,11 @@
+var Wallpaper = require('../models/wallpaper');
+
+exports.index_get = (req, res) =>
+{
+    res.send('NOT IMPLEMENTED: Index GET');
+};
+
+exports.index_post = (req, res) =>
+{
+    res.send('NOT IMPLEMENTED: Index POST');
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+let indexController = require('../controllers/indexController')
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', indexController.index_get);
+
+/* POST home page. */
+router.post('/', indexController.index_post);
 
 module.exports = router;
