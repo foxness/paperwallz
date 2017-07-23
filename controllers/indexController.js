c3936d4d9f8cfcac7ca9657a51884011ef93d92d
foxneZz <foxnezz@gmail.com>
2017-07-23 18:07:21 +0300

    added basic index page / changed port to 80

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,10 +2,17 @@ var Wallpaper = require('../models/wallpaper');
 
 exports.index_get = (req, res) =>
 {
-    res.send('NOT IMPLEMENTED: Index GET');
-};
+    Wallpaper.find()
+    //.sort([['family_name', 'ascending']])
+    .exec((err, list_wallpapers) =>
+    {
+        if (err) { return next(err); }
+        //Successful, so render
+        res.render('index', { wallpaper_list: list_wallpapers });
+    });
+}
 
 exports.index_post = (req, res) =>
 {
     res.send('NOT IMPLEMENTED: Index POST');
-};
+}
