587e921ac784ab10cd2ed85e9350e0ff0660c701
foxneZz <foxnezz@gmail.com>
2017-10-02 22:16:36 +0300

    removed the junk comments

--- models/wallpaper.js
+++ models/wallpaper.js
@@ -10,29 +10,4 @@ let WallpaperSchema = Schema(
     completionDate: { type: Date, required: false }
 })
 
-// WallpaperSchema.pre('update', (next) =>
-// {
-//     this.model('User').update(
-//         { },
-//         { '$pull': { 'queue': this._id } },
-//         { 'multi': true },
-//         next
-//     )
-// })
-
-// // Virtual for author's full name
-// WallpaperSchema
-// .virtual('name')
-// .get(function () {
-//   return this.family_name + ', ' + this.first_name;
-// });
-
-// // Virtual for author's URL
-// WallpaperSchema
-// .virtual('url')
-// .get(function () {
-//   return '/catalog/author/' + this._id;
-// });
-
-//Export model
 module.exports = mongoose.model('Wallpaper', WallpaperSchema)
