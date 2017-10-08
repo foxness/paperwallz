f04bca2af58dcadf055072aef4d367c8f3a58564
foxneZz <foxnezz@gmail.com>
2017-10-08 15:54:17 +0300

    added slider input handling

--- public/javascripts/timer.js
+++ public/javascripts/timer.js
@@ -7,6 +7,45 @@ let destinationDate = null
 let timerPaused = true
 let interval = null
 let updateDelay = 100
+let inputtingWhileNotPaused = false
+
+function sliderInput()
+{
+    let ratio = 1 - $('#slider').val() / $('#slider').prop('max')
+    timeLeft = moment.duration(interval.asMilliseconds() * ratio)
+    
+    if (!timerPaused)
+    {
+        inputtingWhileNotPaused = true
+        destinationDate = moment().add(timeLeft)
+    }
+
+    updateTimerText()
+}
+
+function sliderChange()
+{
+    inputtingWhileNotPaused = false
+}
+
+function updateTimerText()
+{
+    let text = ''
+    let hours = Math.floor(timeLeft.asMilliseconds() / (1000*60*60))
+    text += (hours < 10 ? `0${hours}:` : `${hours}:`)
+    let minutes = timeLeft.minutes()
+    text += (minutes < 10 ? `0${minutes}:` : `${minutes}:`)
+    let seconds = timeLeft.seconds()
+    text += (seconds < 10 ? `0${seconds}` : `${seconds}`)
+
+    $(timerId).text(text)
+}
+
+function updateTimerSlider()
+{
+    let ratio = 1 - timeLeft.asMilliseconds() / interval.asMilliseconds()
+    $('#slider').val(ratio * $('#slider').prop('max'))
+}
 
 function updateTimer(paused_, interval_, info_)
 {
@@ -28,24 +67,15 @@ function updateTimer(paused_, interval_, info_)
 
 function updateInterface()
 {
-    // let text = `${timeLeft.days()}d ${timeLeft.hours()}h ${timeLeft.minutes()}m ${timeLeft.seconds()}s ${timeLeft.milliseconds()}ms`
-    
-    let text = ''
-    let hours = Math.floor(timeLeft.asMilliseconds() / (1000*60*60))
-    text += (hours < 10 ? `0${hours}:` : `${hours}:`)
-    let minutes = timeLeft.minutes()
-    text += (minutes < 10 ? `0${minutes}:` : `${minutes}:`)
-    let seconds = timeLeft.seconds()
-    text += (seconds < 10 ? `0${seconds}` : `${seconds}`)
-
-    let ratio = 1 - timeLeft.asMilliseconds() / interval.asMilliseconds()
-    $('#slider').val(ratio * $('#slider').prop('max'))
-
-    $(timerId).text(text)
+    updateTimerText()
+    updateTimerSlider()
 }
 
 function tick()
 {
+    if (inputtingWhileNotPaused)
+        return
+
     timeLeft = moment.duration(destinationDate.diff(moment()))
 
     if (timeLeft.asMilliseconds() <= 0)
